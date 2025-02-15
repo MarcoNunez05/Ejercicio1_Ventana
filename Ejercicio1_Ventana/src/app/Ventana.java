939054875db6e65fd2e53d13344f3e1081278517
MarcoNunez05 <mnunez_23@alu.uabcs.mx>
2025-02-15 13:12:28 -0700

    Unidad 2 Ejercicio 10: Diseño de Login

--- Ejercicio1_Ventana/src/app/Ventana.java
+++ Ejercicio1_Ventana/src/app/Ventana.java
@@ -3,18 +3,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame 
 {
-	Font fuente = new Font("Serif", Font.PLAIN, 20);
+	Font titulo = new Font("Serif", Font.PLAIN, 32);
+	Font subtitulo = new Font("Serif", Font.PLAIN, 20);
+	Font mini = new Font("Cambria", Font.PLAIN, 14);
 	
 	public Ventana(String title)
 	{
-		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 500);
 		this.setTitle(title);
@@ -25,7 +29,7 @@ public class Ventana extends JFrame
 		
 		this.add(login());
 		this.repaint();
-		
+		this.setVisible(true);
 	}
 	
 	public JPanel login()
@@ -34,18 +38,84 @@ public class Ventana extends JFrame
 		panel.setLocation(0, 0);
 		panel.setSize(this.WIDTH, this.HEIGHT);
 		panel.setOpaque(true);
-		panel.setBackground(new Color(146, 255, 255));
-		
-		JLabel etiqueta = new JLabel("Bienvenido");
-		etiqueta.setSize(160, 40);
-		etiqueta.setLocation(160, 10);
-		etiqueta.setBackground(Color.green);
-		etiqueta.setOpaque(true);
-		etiqueta.setFont(fuente);
-		etiqueta.setHorizontalAlignment(JLabel.CENTER);
+		panel.setLayout(null);
+		panel.setBackground(new Color(159, 252, 238));
 		
 		
-		panel.add(etiqueta);
+		// JLabels
+		
+		JLabel bienvenida = new JLabel("Bienvenido");
+		bienvenida.setSize(160, 40);
+		bienvenida.setLocation(160, 10);
+		bienvenida.setBackground(new Color(95, 238, 250));
+		bienvenida.setOpaque(true);
+		bienvenida.setFont(titulo);
+		bienvenida.setHorizontalAlignment(JLabel.CENTER);
+		
+		JLabel labelNombre = new JLabel("Nombre de usuario:");
+		labelNombre.setSize(180, 30);
+		labelNombre.setLocation(90, 100);
+		labelNombre.setFont(subtitulo);
+		labelNombre.setHorizontalAlignment(JLabel.LEFT);
+		
+		JLabel labelPassword = new JLabel("Contraseña:");
+		labelPassword.setSize(180, 30);
+		labelPassword.setLocation(90, 200);
+		labelPassword.setFont(subtitulo);
+		labelPassword.setHorizontalAlignment(JLabel.LEFT);
+		
+		JLabel olvidar = new JLabel("¿Olvidó su contraseña?");
+		olvidar.setSize(150, 20);
+		olvidar.setLocation(250, 280);
+		olvidar.setFont(mini);
+		olvidar.setHorizontalAlignment(JLabel.LEFT);
+		
+		
+		// JTextField
+		
+		JTextField nombre = new JTextField();
+		nombre.setSize(300, 30);
+		nombre.setLocation(90, 140);
+		nombre.setLayout(null);
+		
+		
+		// JPasswordField
+		
+		JPasswordField password = new JPasswordField();
+		password.setSize(300, 30);
+		password.setLocation(90, 240);
+		password.setLayout(null);
+		
+		
+		// JCheckBox
+		
+		JCheckBox recordar = new JCheckBox("Recordarme");
+		recordar.setSize(120, 20);
+		recordar.setLocation(90, 280);
+		recordar.setOpaque(false);
+		recordar.setLayout(null);
+		recordar.setFont(mini);
+		
+		
+		// JButton
+		
+		JButton acceder = new JButton("Acceder");
+		acceder.setSize(120, 40);
+		acceder.setLocation(180, 340);
+		acceder.setFont(subtitulo);
+		acceder.setLayout(null);
+		
+		
+		// Añadir elementos al panel
+		
+		panel.add(bienvenida);
+		panel.add(labelNombre);
+		panel.add(labelPassword);
+		panel.add(nombre);
+		panel.add(password);
+		panel.add(recordar);
+		panel.add(olvidar);
+		panel.add(acceder);
 		
 		panel.revalidate();
 		return panel;
