126c1fd52dd1fd2d27b6ae53dfc41fcba73c29a0
MarcoNunez05 <mnunez_23@alu.uabcs.mx>
2025-02-18 12:56:55 -0700

    Unidad 2: Ejercicio 14

--- Ejercicio1_Ventana/src/app/Ventana.java
+++ Ejercicio1_Ventana/src/app/Ventana.java
@@ -3,12 +3,17 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Ventana extends JFrame 
@@ -20,14 +25,15 @@ public class Ventana extends JFrame
 	public Ventana(String title)
 	{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(500, 500);
+		this.setSize(500, 800);
 		this.setTitle(title);
 		this.setLocationRelativeTo(null);
 		
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setMaximumSize(new Dimension(600, 600));
 		
-		this.add(login());
+		// this.add(login());
+		this.add(register());
 		this.repaint();
 		this.setVisible(true);
 	}
@@ -121,4 +127,150 @@ public class Ventana extends JFrame
 		return panel;
 	}
 	
+	
+	public JPanel register()
+	{
+		JPanel panel = new JPanel();
+		panel.setLocation(0, 0);
+		panel.setSize(this.WIDTH, this.HEIGHT);
+		panel.setOpaque(true);
+		panel.setLayout(null);
+		panel.setBackground(new Color(152, 190, 250));
+		
+		// JLabels
+		
+		JLabel registro = new JLabel("Registro");
+		registro.setSize(160, 40);
+		registro.setLocation(160, 10);
+		registro.setBackground(new Color(83, 143, 237));
+		registro.setOpaque(true);
+		registro.setFont(titulo);
+		registro.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(registro);
+		
+		JLabel labelNombre = new JLabel("Nombre de usuario:");
+		labelNombre.setSize(190, 30);
+		labelNombre.setLocation(165, 90);
+		labelNombre.setFont(subtitulo);
+		labelNombre.setHorizontalAlignment(JLabel.LEFT);
+		panel.add(labelNombre);
+		
+		JLabel labelBio = new JLabel("Escriba una breve biografía:");
+		labelBio.setSize(250, 30);
+		labelBio.setLocation(130, 190);
+		labelBio.setFont(subtitulo);
+		labelBio.setHorizontalAlignment(JLabel.LEFT);
+		panel.add(labelBio);
+		
+		JLabel labelPreferencia = new JLabel("Escoja sus preferencias:");
+		labelPreferencia.setSize(250, 30);
+		labelPreferencia.setLocation(150, 355);
+		labelPreferencia.setFont(subtitulo);
+		labelPreferencia.setHorizontalAlignment(JLabel.LEFT);
+		panel.add(labelPreferencia);
+		
+		JLabel labelTerminos = new JLabel("¿Usted acepta los términos?");
+		labelTerminos.setSize(250, 30);
+		labelTerminos.setLocation(130, 455);
+		labelTerminos.setFont(subtitulo);
+		labelTerminos.setHorizontalAlignment(JLabel.LEFT);
+		panel.add(labelTerminos);
+		
+		JLabel labelColonia = new JLabel("Elija su colonia:");
+		labelColonia.setSize(250, 30);
+		labelColonia.setLocation(175, 550);
+		labelColonia.setFont(subtitulo);
+		labelColonia.setHorizontalAlignment(JLabel.LEFT);
+		panel.add(labelColonia);
+		
+		
+		// JTextField
+		
+		JTextField nombre = new JTextField();
+		nombre.setSize(300, 30);
+		nombre.setLocation(90, 130);
+		nombre.setLayout(null);
+		panel.add(nombre);
+		
+		
+		// JTextArea
+		
+		JTextArea bio = new JTextArea();
+		bio.setSize(400, 100);
+		bio.setLocation(40, 230);
+		bio.setLayout(null);
+		panel.add(bio);
+		
+		
+		// JCheckBox
+		
+		JCheckBox dulces = new JCheckBox("Dulces");
+		dulces.setSize(120, 20);
+		dulces.setLocation(100, 395);
+		dulces.setOpaque(false);
+		dulces.setLayout(null);
+		dulces.setFont(mini);
+		panel.add(dulces);
+		
+		JCheckBox salado = new JCheckBox("Salado");
+		salado.setSize(120, 20);
+		salado.setLocation(210, 395);
+		salado.setOpaque(false);
+		salado.setLayout(null);
+		salado.setFont(mini);
+		panel.add(salado);
+		
+		JCheckBox saludable = new JCheckBox("Saludable");
+		saludable.setSize(120, 20);
+		saludable.setLocation(315, 395);
+		saludable.setOpaque(false);
+		saludable.setLayout(null);
+		saludable.setFont(mini);
+		panel.add(saludable);
+		
+		
+		// JRadioButton
+		
+		ButtonGroup terminos = new ButtonGroup();
+		
+		JRadioButton terms1 = new JRadioButton("Acepto los términos", true);
+		terms1.setLocation(80, 485);
+		terms1.setSize(180, 40);
+		terms1.setOpaque(false);
+		terms1.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel.add(terms1);
+		
+		JRadioButton terms2 = new JRadioButton("Rechazo los términos");
+		terms2.setLocation(260, 485);
+		terms2.setSize(180, 40);
+		terms2.setOpaque(false);
+		terms2.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel.add(terms2);
+		
+		terminos.add(terms1);
+		terminos.add(terms2);
+		
+		// JComboBox
+		
+		String [] coloniasDataSet = {"Centro", "Villas del encanto", "Pedregal", "Agua escondida", "Balandra"};
+		
+		JComboBox colonias = new JComboBox(coloniasDataSet);
+		colonias.setLocation(150, 590);
+		colonias.setSize(180, 40);
+		colonias.setLayout(null);
+		panel.add(colonias);
+		
+		
+		// JButton
+		
+		JButton registrarse = new JButton("Crear cuenta");
+		registrarse.setSize(300, 50);
+		registrarse.setLocation(90, 670);
+		registrarse.setFont(subtitulo);
+		registrarse.setLayout(null);
+		panel.add(registrarse);
+		
+		panel.revalidate();
+		return panel;
+	}
 }
