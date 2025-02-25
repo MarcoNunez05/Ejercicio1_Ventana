742b8f82b40a84be4d2c22a7f4822240697dcfaf
MarcoNunez05 <mnunez_23@alu.uabcs.mx>
2025-02-24 21:04:19 -0700

    Unidad 2 Ejercicio 19: Diseño de login

--- Ejercicio1_Ventana/src/app/Ventana.java
+++ Ejercicio1_Ventana/src/app/Ventana.java
@@ -1,8 +1,11 @@
 package app;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -14,14 +17,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
 
 public class Ventana extends JFrame 
 {
-	Font titulo = new Font("Serif", Font.PLAIN, 32);
-	Font subtitulo = new Font("Serif", Font.PLAIN, 20);
-	Font mini = new Font("Cambria", Font.PLAIN, 14);
+	Font titulo = new Font("Cambria", Font.BOLD, 32);
+	Font subtitulo = new Font("Cambria Math", Font.PLAIN, 20);
+	Font mini = new Font("Cambria Math", Font.PLAIN, 14);
 	
 	Font calculadoraFontResultado = new Font("Cambria", Font.PLAIN, 32);
 	Font calculadoraFont = new Font("Cambria", Font.PLAIN, 20);
@@ -29,108 +36,198 @@ public class Ventana extends JFrame
 	public Ventana(String title)
 	{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(500, 500);
+		this.setSize(800, 700);
 		this.setTitle(title);
 		this.setLocationRelativeTo(null);
 		
 		this.setMinimumSize(new Dimension(400, 400));
 		
-		// this.add(login());
+		this.add(login());
 		// this.add(register());
-		this.add(calculadora());
+		// this.add(calculadora());
+		// this.add(tablaRegistros());
 		this.repaint();
 		this.setVisible(true);
 	}
 	
 	public JPanel login()
 	{
-		// Size de ventana recomendada: (500, 500)
+		// Size de ventana recomendada: (800, 700)
 		
-		JPanel panel = new JPanel();
-		panel.setLocation(0, 0);
-		panel.setSize(this.WIDTH, this.HEIGHT);
-		panel.setOpaque(true);
-		panel.setLayout(null);
-		panel.setBackground(new Color(159, 252, 238));
+		JPanel panelAfuera = new JPanel();
+		panelAfuera.setLocation(0, 0);
+		panelAfuera.setSize(this.WIDTH, this.HEIGHT);
+		panelAfuera.setOpaque(true);
+		panelAfuera.setBackground(new Color(61, 98, 219));
+		panelAfuera.setLayout(null);
+		
+		JPanel panelAdentro = new JPanel();
+		panelAdentro.setLocation(160, 90);
+		panelAdentro.setSize(470, 450);
+		panelAdentro.setOpaque(true);
+		panelAdentro.setLayout(null);
+		panelAdentro.setBackground(Color.white);
+		panelAdentro.setBorder(new LineBorder(new Color(16, 47, 125), 6));
 		
 		
 		// JLabels
 		
-		JLabel bienvenida = new JLabel("Bienvenido");
-		bienvenida.setSize(160, 40);
-		bienvenida.setLocation(160, 10);
-		bienvenida.setBackground(new Color(95, 238, 250));
+		JLabel bienvenida = new JLabel("Bienvenido/a");
+		bienvenida.setSize(210, 40);
+		bienvenida.setLocation(285, 10);
+		bienvenida.setBackground(new Color(43, 107, 217));
+		bienvenida.setForeground(Color.white);
 		bienvenida.setOpaque(true);
 		bienvenida.setFont(titulo);
 		bienvenida.setHorizontalAlignment(JLabel.CENTER);
+		panelAfuera.add(bienvenida);
 		
-		JLabel labelNombre = new JLabel("Nombre de usuario:");
-		labelNombre.setSize(180, 30);
+		JLabel accede = new JLabel("Accede con tu cuenta");
+		accede.setSize(320, 40);
+		accede.setLocation(75, 15);
+		accede.setFont(titulo);
+		accede.setHorizontalAlignment(JLabel.CENTER);
+		panelAdentro.add(accede);
+		
+		JLabel labelNombre = new JLabel("E-mail o nombre de usuario:");
+		labelNombre.setSize(250, 20);
 		labelNombre.setLocation(90, 100);
 		labelNombre.setFont(subtitulo);
 		labelNombre.setHorizontalAlignment(JLabel.LEFT);
+		panelAdentro.add(labelNombre);
 		
 		JLabel labelPassword = new JLabel("Contraseña:");
-		labelPassword.setSize(180, 30);
-		labelPassword.setLocation(90, 200);
+		labelPassword.setSize(180, 20);
+		labelPassword.setLocation(90, 210);
 		labelPassword.setFont(subtitulo);
 		labelPassword.setHorizontalAlignment(JLabel.LEFT);
+		panelAdentro.add(labelPassword);
 		
 		JLabel olvidar = new JLabel("¿Olvidó su contraseña?");
 		olvidar.setSize(150, 20);
-		olvidar.setLocation(250, 280);
+		olvidar.setLocation(250, 290);
 		olvidar.setFont(mini);
+		olvidar.setForeground(new Color(78, 118, 245));
 		olvidar.setHorizontalAlignment(JLabel.LEFT);
+		panelAdentro.add(olvidar);
+		
+		JLabel labelRegistro = new JLabel("¿No tienes una cuenta?");
+		labelRegistro.setSize(150, 20);
+		labelRegistro.setLocation(325, 560);
+		labelRegistro.setFont(mini);
+		labelRegistro.setForeground(Color.white);
+		labelRegistro.setHorizontalAlignment(JLabel.LEFT);
+		panelAfuera.add(labelRegistro);
 		
 		
 		// JTextField
 		
 		JTextField nombre = new JTextField();
-		nombre.setSize(300, 30);
-		nombre.setLocation(90, 140);
+		nombre.setSize(300, 40);
+		nombre.setLocation(90, 130);
 		nombre.setLayout(null);
+		nombre.setBorder(new LineBorder(Color.black, 3, true));
+		panelAdentro.add(nombre);
+		
 		
 		
 		// JPasswordField
 		
 		JPasswordField password = new JPasswordField();
-		password.setSize(300, 30);
+		password.setSize(300, 40);
 		password.setLocation(90, 240);
 		password.setLayout(null);
+		password.setBorder(new LineBorder(Color.black, 3, true));
+		panelAdentro.add(password);
 		
 		
 		// JCheckBox
 		
 		JCheckBox recordar = new JCheckBox("Recordarme");
 		recordar.setSize(120, 20);
-		recordar.setLocation(90, 280);
+		recordar.setLocation(90, 290);
 		recordar.setOpaque(false);
 		recordar.setLayout(null);
 		recordar.setFont(mini);
+		panelAdentro.add(recordar);
 		
 		
 		// JButton
 		
 		JButton acceder = new JButton("Acceder");
-		acceder.setSize(120, 40);
-		acceder.setLocation(180, 340);
+		acceder.setSize(130, 50);
+		acceder.setLocation(170, 350);
+		acceder.setBackground(new Color(11, 156, 154));
+		acceder.setForeground(Color.white);
 		acceder.setFont(subtitulo);
 		acceder.setLayout(null);
+		acceder.setBorder(new LineBorder(Color.black, 3, true));
+		panelAdentro.add(acceder);
+		
+		JButton registrar = new JButton("Regístrate");
+		registrar.setSize(130, 50);
+		registrar.setLocation(330, 590);
+		registrar.setBackground(new Color(11, 60, 219));
+		registrar.setForeground(Color.white);
+		registrar.setFont(subtitulo);
+		registrar.setLayout(null);
+		registrar.setBorder(new LineBorder(Color.black, 3, true));
+		panelAfuera.add(registrar);
+		
+		// Le ponemos un evento de Mouse a los botones
+		
+		acceder.addMouseListener(new MouseAdapter() 
+		{
+			public void mouseEntered(MouseEvent e)
+			{
+				acceder.setBackground(new Color(97, 186, 186));
+			}
+			
+			public void mouseExited(MouseEvent e)
+			{
+				acceder.setBackground(new Color(11, 156, 154));
+			}
+		});
+		
+		registrar.addMouseListener(new MouseAdapter() 
+		{
+			public void mouseEntered(MouseEvent e)
+			{
+				registrar.setBackground(new Color(41, 83, 217));
+			}
+			
+			public void mouseExited(MouseEvent e)
+			{
+				registrar.setBackground(new Color(11, 60, 219));
+			}
+		});
 		
 		
-		// Añadir elementos al panel
 		
-		panel.add(bienvenida);
-		panel.add(labelNombre);
-		panel.add(labelPassword);
-		panel.add(nombre);
-		panel.add(password);
-		panel.add(recordar);
-		panel.add(olvidar);
-		panel.add(acceder);
+		// ImageIcon
 		
-		panel.revalidate();
-		return panel;
+		JLabel imagenUsuario = new JLabel(new ImageIcon(getClass().getResource("images/user.png")));
+		imagenUsuario.setSize(32, 32);
+		imagenUsuario.setLocation(50, 133);
+		panelAdentro.add(imagenUsuario);
+		
+		JLabel imagenClave = new JLabel(new ImageIcon(getClass().getResource("images/lock.png")));
+		imagenClave.setSize(32, 32);
+		imagenClave.setLocation(50, 242);
+		panelAdentro.add(imagenClave);
+		
+		JLabel imagenOjo = new JLabel(new ImageIcon(getClass().getResource("images/eye.png")));
+		imagenOjo.setSize(32, 32);
+		imagenOjo.setLocation(400, 244);
+		panelAdentro.add(imagenOjo);
+		
+
+		panelAdentro.revalidate();
+		
+		panelAfuera.add(panelAdentro);
+		
+		panelAfuera.revalidate();
+		return panelAfuera;
 	}
 	
 	
@@ -531,6 +628,119 @@ public class Ventana extends JFrame
 		mas.setBorderPainted(false);
 		panelGrid.add(mas);
 		
+		
+		panel.revalidate();
+		return panel;
+	}
+	
+	public JPanel tablaRegistros()
+	{
+		// Size de la ventana recomendada: (1000, 800)
+		
+		JPanel panel = new JPanel();
+		panel.setLocation(0, 0);
+		panel.setSize(this.WIDTH, this.HEIGHT);
+		panel.setOpaque(true);
+		panel.setLayout(null);
+		panel.setBackground(new Color(200, 180, 237));
+		
+				
+		// JLabels
+				
+		JLabel labelUsuarios = new JLabel("Usuarios");
+		labelUsuarios.setSize(160, 40);
+		labelUsuarios.setLocation(420, 10);
+		labelUsuarios.setBackground(new Color(153, 151, 247));
+		labelUsuarios.setOpaque(true);
+		labelUsuarios.setFont(titulo);
+		labelUsuarios.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(labelUsuarios);
+		
+		JLabel totalUsuarios = new JLabel("Total de usuarios registrados: 100");
+		totalUsuarios.setSize(300, 40);
+		totalUsuarios.setLocation(350, 80);
+		totalUsuarios.setOpaque(true);
+		totalUsuarios.setFont(subtitulo);
+		totalUsuarios.setBorder(BorderFactory.createLineBorder(Color.black));
+		totalUsuarios.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(totalUsuarios);
+		
+		
+		// JButtons
+		
+		JButton exportar = new JButton("Exportar");
+		exportar.setSize(150, 40);
+		exportar.setLocation(335, 670);
+		exportar.setFont(subtitulo);
+		exportar.setLayout(null);
+		panel.add(exportar);
+		
+		JButton agregar = new JButton("Añadir");
+		agregar.setSize(150, 40);
+		agregar.setLocation(510, 670);
+		agregar.setFont(subtitulo);
+		agregar.setLayout(null);
+		panel.add(agregar);
+		
+		
+		// JTable
+		
+		String[] nombreColumnas = {
+				"ID",
+				"Nombre",
+				"Correo Electrónico",
+				"Edad",
+				"Juego"
+				};
+		
+		Object[][] datos = {
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"},
+				{"ID", "Nombre", "Correo", "Edad", "Juego"}};
+					
+		
+		JTable tabla = new JTable(datos, nombreColumnas);
+		JScrollPane scrollPane = new JScrollPane(tabla);
+		scrollPane.setLocation(30, 150);
+		scrollPane.setSize(920, 500);
+		panel.add(scrollPane);
 		
 		panel.revalidate();
 		return panel;
