32a2abd4816a587aaffdfdf2ed6ade772c912a46
MarcoNunez05 <mnunez_23@alu.uabcs.mx>
2025-03-18 09:42:37 -0700

    Actualización de Main para Unidad 3 Ejercicio 9 y 10

--- Ejercicio1_Ventana/src/app/Main.java
+++ Ejercicio1_Ventana/src/app/Main.java
@@ -6,10 +6,10 @@ public class Main {
 	{
 		// TODO Auto-generated method stub
 		
-		// Ventana ventana = new Ventana("Ventana");
+		Ventana ventana = new Ventana("Ventana");
 		// VentanaMarioBros3 ventanaMario3 = new VentanaMarioBros3("Super Mario Bros 3.");
 		// VentanaMarioWorld ventanaMarioWorld = new VentanaMarioWorld("Super Mario World");
-		VentanaExamen2 examen = new VentanaExamen2("");
+		// VentanaExamen2 examen = new VentanaExamen2("");
 	}
 
 }
